a9a3afd89a7507f8c35de4ddd08f554a295ea7d0
Yoon Hee Cho <yoonheecho1123@gmail.com>
2020-08-17 11:23:51 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log("hello this is index.js in src folder");
+
+//webpack will gathered all the js file in the src folder
+// into one bundle.js and auto save it in the public folder
+// so index.html only put bundle.js in script tag
+// no need to mention js files in src folder
